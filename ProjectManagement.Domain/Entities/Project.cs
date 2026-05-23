666d76086ab1ffc8344530629ec81c1bdd19aaf6
kerolos <kerolosnesim11@gmail.com>
2026-05-23 18:12:33 +0300

    chore: initialize .NET 9 Web API project with Clean Architecture structure

---
+++ ProjectManagement.Domain/Entities/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Domain.Entities
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ ProjectManagement.Domain/Entities/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using ProjectManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectManagement.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+        public TaskPriority Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
